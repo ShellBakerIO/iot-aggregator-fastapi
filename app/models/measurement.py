2161b0a26238de25f396b6762c5a0f1ae1ac1cae
Fariz Nasibov <shellbaker123@gmail.com>
2025-02-07 22:18:47 +0500

    feat: primary foreign key connection

--- app/models/measurement.py
+++ app/models/measurement.py
@@ -1,4 +1,4 @@
-from sqlalchemy import String, DateTime
+from sqlalchemy import DateTime, ForeignKey
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 
@@ -10,6 +10,6 @@ class Measurement(Base):
     __tablename__ = 'measurement'
 
     id: Mapped[int] = mapped_column(auto_increment=True)
-    device_id: Mapped[int] = mapped_column(foreign_key=True)
-    value: Mapped[str] = mapped_column(String)
+    device_id: Mapped[int] = mapped_column(ForeignKey('devices.id'))
+    value: Mapped[str]
     timestamp: Mapped[DateTime] = mapped_column(DateTime)
